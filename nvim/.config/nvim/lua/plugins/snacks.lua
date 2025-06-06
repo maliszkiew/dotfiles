ddff2dd5341bb53ca705b6df54ef6dcd2c0e9aae
Igor Maliszkiewicz <s193091@student.pg.edu.pl>
2025-06-06 21:33:12 +0200

    u

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,23 +1,24 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		explorer = {
-			enabled = true,
-			auto_close = false, -- Prevents closing when opening files
-			follow_file = true, -- Follows current file
-			focus = "list", -- Focus on the file list
-			jump = { close = false },
-			replace_ntrw = true,
-		},
-	},
-	config = function(_, opts)
-		require("snacks").setup(opts)
-		-- Autocmd to resize explorer window
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "snacks-explorer", -- Adjust if needed
-			callback = function()
-				vim.cmd("vertical resize 15")
-			end,
-		})
-	end,
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          auto_close = true,
+          hidden = false,
+          layout = {
+            preset = "default", -- Use sidebar layout
+            preview = false,
+            -- THIS IS THE KEY: set width here
+            layout = {
+              width = 10, -- Set to your desired sidebar width
+            },
+          },
+        },
+      },
+    },
+  },
 }
