e0ad874f826f4a12b6381436186e371b98e85c12
Igor Maliszkiewicz <s193091@student.pg.edu.pl>
2025-06-07 17:22:30 +0200

    u

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,18 +7,48 @@ return {
       enabled = true,
       sources = {
         explorer = {
-          auto_close = true,
+          auto_close = false,
           hidden = false,
           layout = {
-            preset = "default", -- Use sidebar layout
+            preset = "sidebar",
             preview = false,
-            -- THIS IS THE KEY: set width here
-            layout = {
-              width = 10, -- Set to your desired sidebar width
-            },
+            layout = { width = 20 },
           },
         },
+        projects = {
+          -- THIS is where you put your patterns and dev directories!
+          dev = { "~/repos" },
+          patterns = { ".git", "CMakeLists.txt", "Makefile", "venv" },
+        },
       },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        local explorer_pickers = Snacks.picker.get({ source = "explorer" })
+        for _, v in pairs(explorer_pickers) do
+          if v:is_focused() then
+            v:close()
+          else
+            v:focus()
+          end
+        end
+        if #explorer_pickers == 0 then
+          Snacks.picker.explorer()
+        end
+      end,
+      desc = "Toggle Snacks Explorer",
+      mode = { "n" },
+    },
+    {
+      "<leader>p",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Open Snacks Projects Picker",
+      mode = { "n" },
     },
   },
 }
