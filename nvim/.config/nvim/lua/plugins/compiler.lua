0a5c0e2d1cb5e524dde51f51645014f341842e89
Igor Maliszkiewicz <s193091@student.pg.edu.pl>
2025-06-13 00:06:56 +0200

    u

--- nvim/.config/nvim/lua/plugins/compiler.lua
+++ nvim/.config/nvim/lua/plugins/compiler.lua
@@ -1,11 +1,11 @@
 return {
-  { -- This plugin
+  {
     "Zeioth/compiler.nvim",
     cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
     dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
     opts = {},
   },
-  { -- The task runner we use
+  {
     "stevearc/overseer.nvim",
     commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
     cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
